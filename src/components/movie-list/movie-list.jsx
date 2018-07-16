189d8a456d3cd7f9a94b6ba41ef541a474c4120b
cav2008 <cavan.law@bt.com>
2018-07-16 11:12:45 +0100

    remove commented out code

--- src/components/movie-list/movie-list.jsx
+++ src/components/movie-list/movie-list.jsx
@@ -3,21 +3,6 @@ import PropTypes from 'prop-types';
 
 import MovieItem from '../movie-item/movie-item';
 
-// export default class MovieList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     console.log(this.props);
-//   }
-
-//   render() {
-//     return (
-//       <div className="movie-list">
-//         {this.props.movies.map((movie, index) => <MovieItem key={index} movie={movie} />)}
-//       </div>
-//     );
-//   }
-// }
-
 const MovieList = (props) => {
   return (
     <div className="movie-list">
